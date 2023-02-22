716bcc192f5d40252377b15cda2c86be5d1996c5
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-02-22 16:34:01 +0100

    initialCommit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,96 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Videos</h1>
+
+    <div class="video-container">
+      <div v-for="video in videos" :key="video">
+        <div class="video-box">
+          <img :src="video.thumbnail"/>
+          <div>
+            <h3>{{video.name}}</h3>
+            <div v-html="video.description"></div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      img: require("@/assets/dog.jpeg"),
+      videos: [
+        {
+          name: "1",
+          description: "<p>thjfuydghdjdhdgdhyyhfhjfhf</p>",
+          thumbnail: require("@/assets/dog.jpeg")
+        },
+        {
+          name: "2",
+          description: ""
+        },
+        {
+          name: "3",
+          description: ""
+        },
+        {
+          name: "4",
+          description: ""
+        },
+        {
+          name: "5",
+          description: ""
+        },
+        {
+          name: "6",
+          description: ""
+        },
+        {
+          name: "7",
+          description: ""
+        },
+        {
+          name: "8",
+          description: ""
+        },
+        // {
+        //   name: "",
+        //   description: ""
+        // }
+      ]
+
+    }
   }
 }
 </script>
+
+<style scoped>
+.video-box {
+  border: 1px solid black;
+  border-radius: 10px;
+  margin: 10px;
+  padding: 10px;
+
+  text-align: left;
+
+  display: flex;
+  justify-content: flex-start;
+
+}
+
+img {
+  max-height: 150px;
+  padding: 10px;
+}
+
+</style>
---
+++ src/views/VideoWatch.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Lets watch a video!!!</h1>
+</template>
+
+<script>
+export default {
+  name: "VideoWatch"
+}
+</script>
+
+<style scoped>
+
+</style>
